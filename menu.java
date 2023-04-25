eeae48abf98c2065ebdfe25c7908b845bdee3284
ErinTomorri <97967950+ErinTomorri@users.noreply.github.com>
2023-04-24 18:15:16 -0700

    Create menu.java

---
+++ menu.java
@@ -0,0 +1,33 @@
+package Objects;
+public class menu {
+	
+	protected double price;
+	protected String name;
+	protected int cals;
+	protected int protein;
+	protected int carb;
+	protected int fat;
+	protected int numOfItem;
+	public double getPrice() {
+		return price;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getCals() {
+		return cals;
+	}
+	public int getFat() {
+		return fat;
+	}
+	public int getProtien() {
+		return protein;
+	}
+	public int getCarb() {
+		return carb;
+	}
+	public int getNumOfItem() {
+		return cals;
+	}
+}
+
